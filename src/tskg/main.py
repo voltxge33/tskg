b83d2a4a5384eeb1be4cd03db7cb3bf44d03c1b6
Volt <voltxge3@gmail.com>
2025-07-13 19:44:56 -0400

    Added Priorities and fixed some stuff

--- src/tskg/main.py
+++ src/tskg/main.py
@@ -10,7 +10,6 @@ DATA_FILE = DATA_DIR / "tasks.json"
 # TYPER MY BELOVED
 app = typer.Typer(help="A simple To-Do list for even simpler people", no_args_is_help=True)
 # Variables so I don't have to type the same exact fucking thing 10 times
-
 completed = " | Completed!"
 incomplete = " | Not Completed"
 
@@ -29,24 +28,35 @@ def save_tasks(tasks):
 
 # Adds a task
 @app.command()
-def add(task: str = typer.Argument(..., help="The name of the task you want to add"), note: str = typer.Option("", help='If you want to add a specific note to further describe')):
+def add(task: str = typer.Argument(..., help="The name of the task you want to add"),
+        note: str = typer.Option("", help='If you want to add a specific note to further describe'),
+        priority: str = typer.Option("", help="Priorities (High, Medium, Low, H, M, L)")):
     """
     Adds a task to the To-Do list (Cannot be an existing task)
-
-    Optional: --note Add a little note that shows when you use the list command
     """
+    if priority:
+        if str.lower(priority) in ('h', 'high'):
+            priority = "High"
+        elif str.lower(priority) in ("l", 'low'):
+            priority = "Low"
+        elif str.lower(priority) in ("m", "medium"):
+            priority = "Medium"
+        else:
+            print(f"{priority} is not a correct priority level (L,M,H, Low, Medium, High)")
+            return
     tasks = load_tasks()
     new_task = {
         "name": task,
         "status": incomplete,
-        "note": note
+        "note": note,
+        "priority": priority
     }
-    if task in tasks:
-        print("Task has already been added, pick another name")
-    else:
-        print(f"Added {task} to list")
-        tasks.append(new_task)
-        save_tasks(tasks)
+    if any(t["name"].lower() == task.lower() for t in tasks):
+        print(f"{task} already in list, please try again.")
+        return
+    tasks.append(new_task)
+    save_tasks(tasks)
+    print(f'{task} added to the list.')
 
 # Removes a task from the list
 @app.command()
@@ -85,8 +95,8 @@ def complete(task: str):
             else:
                 print("Task not removed.")
             save_tasks(tasks)
-        else:
-            print(f"{task} not found in list.")
+            return
+    print(f"{task} not found in list.")
 
 
 # Lists all current tasks
@@ -99,10 +109,16 @@ def list():
     if tasks:
         print("Tasks:")
         for tid, task in enumerate(tasks, start=1):
-            if task["note"] == "":
-                print(f"{tid}. {task['name']}{task['status']}")
+            if task["note"] != "" and task["priority"] != "":
+                print(f"{tid}. {task['name']}{task['status']}\nNote: {task['note']}\nPriority: {task['priority']}")
+            elif task["note"] != "":
+                print(f"{tid}. {task['name']}{task['status']}\nNote: {task['note']}")
+            elif task["priority"] != "":
+                print(f"{tid}. {task['name']}{task['status']}\nPriority: {task['priority']}")
             else:
-                print(f"{tid}. {task['name']}{task['status']}\nNote: {task["note"]}")
+                print(f"{tid}. {task['name']}{task['status']}")
     else:
         print("No tasks found")
 
+if __name__ == "__main__":
+    app()
